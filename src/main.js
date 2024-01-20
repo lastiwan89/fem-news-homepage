45270bb8949ed783aa7edc7fb542aca5e924f33b
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-20 15:37:58 +0900

    done 80

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 const menu = document.getElementById("menu");
+const bars = document.getElementById("burger");
+const close = document.getElementById("close");
+const menuItem = document.getElementById("menu-item");
 
 menu.addEventListener("click", () => {
   console.log("this is menu item and worked!!");
+  if (menu) {
+    bars.classList.toggle("active");
+    close.classList.toggle("active-close");
+    menuItem.classList.toggle("item-close");
+  }
 });
